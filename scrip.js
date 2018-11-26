c7b79414d61c6534c3cd85221717e1412046fbb3
Kenedy Vargas <35229437+KaVargas@users.noreply.github.com>
2018-11-26 12:19:45 -0500

    Add pop function on clientes:Array, started by the last element
    Create a new funtion to print Array, and a controller to lenght of clientes:Array

--- scrip.js
+++ scrip.js
@@ -1,14 +1,30 @@
 
 var numeroClientes = Math.floor(Math.random() * 31);
+var cli = new Array();
+var movies = [
+    "Bohemian rhapsody",
+    "Venon",
+    "Avengers 4",
+    "Toy story 4",
+    "Animales fantasticos"
+];
+var edad;
+var precio;
+var pelicula;
+var recaudacion = [0, 0, 0, 0, 0];
 
-function clientes(numeroClientes) {
-    var cli = new Array();
-    var tex = "";
-    for (var i = 1; i < numeroClientes + 1; i++) {
-        cli.push("cliente " + i);
+function generarClientes(numeroClientes) {
+
+    for (var i = 0; i < numeroClientes; i++) {
+        cli.push("cliente " + (numeroClientes - i));
     }
-    cli.forEach(imprimir);
-    document.getElementById("clientes").innerHTML = tex;
+    imprimirArray(cli, "clientes");
+}
+
+function imprimirArray(lista, identificador) {
+    var tex = "";
+    lista.forEach(imprimir);
+    document.getElementById(identificador).innerHTML = tex;
 
     function imprimir(value) {
         tex = tex + value + "<br>";
@@ -16,10 +32,9 @@ function clientes(numeroClientes) {
 }
 
 
-
 function generar_edad_precio() {
-    var edad = Math.floor(Math.random() * 66) + 5;
-    var precio;
+    edad = Math.floor(Math.random() * 66) + 5;
+    precio;
     document.getElementById("edad").innerHTML = edad;
 
     if (edad >= 5 && edad <= 10) {
@@ -37,19 +52,28 @@ function generar_edad_precio() {
 }
 
 function generar_peli() {
-    var movies = [
-        "Bohemian rhapsody",
-        "Venon",
-        "Avengers 4",
-        "Toy story 4",
-        "Animales fantasticos"
-    ];
     var npeli = Math.floor(Math.random() * 5);
+    pelicula = movies[npeli];
     document.getElementById("pelicula").innerHTML = movies[npeli];
 }
 
-function main(){
-    clientes(numeroClientes);
+function comprar() {
+    if (cli.length >= 1) {
+        cli.pop();
+        imprimirArray(cli, "clientes");
+        generar_edad_precio();
+        generar_peli();
+        var numero_pelicula = movies.indexOf(pelicula);
+        recaudacion[numero_pelicula] += precio;
+
+    }
+    else{
+        alert("Ya no hay clientes, buen trabajo!");
+    }
+}
+
+function main() {
+    generarClientes(numeroClientes);
     generar_edad_precio();
     generar_peli();
 
